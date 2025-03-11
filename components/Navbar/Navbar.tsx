f4d4321ad1931438af692c8bc14c40d7f80d777e
Game <hdhsdhdhdd1@gmail..com>
2025-03-11 17:36:18 +0700

    yes

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import Logo from "./Logo"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <Logo />
+        </div>
+        <hr />
+    </nav>
+    
+  )
+}
+export default Navbar
