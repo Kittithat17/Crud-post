14a41e8875a046e6c2abc2da572b5c5325b958df
JakiEz <Jackietf4@gail.com>
2025-03-21 01:24:13 +0700

    try home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
+"use client"
 import Autoslides from "@/components/Autoslide/Autoslide"
+import Cardd from "../components/card"
+import { useEffect, useState } from "react"
+
+// const [product, setProduct] = useState([]);
 
 const page = () => {
+  const product = [
+    {title:"addidas"},
+    {title:"vans"},
+    {title:"nike"},
+    {title:"converse"},
+    {title:"converse"},
+    {title:"converse"},
+    {title:"converse"},
+    {title:"converse"},
+    {title:"converse"},
+
+  ]
   return (
     <div>
       <Autoslides />
+      <div className="flex justify-center gap-4 mt-8">
+        {product?.map((item)=> (
+          <button className="border border-black rounded-full hover:bg-black hover:text-white h-[50px] w-[120px] " key={item?.title}>
+            {item?.title}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-4 center">
+        <Cardd/>
+        <Cardd/>
+        <Cardd/>
+        <Cardd/>
+      </div>
     </div>
   )
 }
+
 export default page
---
+++ components/card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className ="bg-black opacity-30 h-[180px] w-[140px] rounded-xl text-amber-100">
+        <img src=""/>
+    </div>
+  )
+}
+
+export default card
