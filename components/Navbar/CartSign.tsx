7ae1afce4893d674d9fa4b3cba3f6848b286a4ae
Kittithat17 <hdhsdhdhdd1@gmail.com>
2025-05-04 00:20:23 +0700

    sssd

--- components/Navbar/CartSign.tsx
+++ components/Navbar/CartSign.tsx
@@ -17,7 +17,7 @@ const CartSign = () => {
   }, [getTotalItems]);
 
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-3">
       <Button asChild variant={"ghost"} size={'lg'}>
         <Link href="/cart" className="relative flex items-center">
           <ShoppingBag className="size-7" />
