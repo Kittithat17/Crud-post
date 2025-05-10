3d0df4a351bf588f28a024173298eb96e38258f0
JakiEz <Jackietf4@gail.com>
2025-05-10 22:37:16 +0700

    DDWADWADWADAW

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,16 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'www.converse.co.th',
         pathname: '/**'
+      },
+        {
+        protocol: 'https',
+        hostname: "image.goat.com",
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: "i.ebayimg.com",
+        pathname: '/**'
       }
       
       
